f8ee61b4a92df965762a6a28069322e02cda811c
jakob-bjorner <jbjorner3@gatech.edu>
2021-10-24 00:53:58 -0400

    fix styling

--- binit/src/Nav.js
+++ binit/src/Nav.js
@@ -1,50 +1,56 @@
-import React, { useState } from 'react';
-import './App.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
-    const[findBinsButtonColor,setFindBinsButtonColor] = useState();
-    const[addBinsButtonColor,setAddBinsButtonColor] = useState();
+  const [findBinsButtonColor, setFindBinsButtonColor] = useState();
+  const [addBinsButtonColor, setAddBinsButtonColor] = useState();
 
-    function changeFindButtonColor(){
-        if(findBinsButtonColor == "#515F30") {
-            setFindBinsButtonColor("#8BA352");
-            setAddBinsButtonColor("#515F30");
-        } else {
-            setFindBinsButtonColor("#515F30");
-            setAddBinsButtonColor("#8BA352");
-        }
+  function changeFindButtonColor() {
+    if (findBinsButtonColor == "#515F30") {
+      setFindBinsButtonColor("#8BA352");
+      setAddBinsButtonColor("#515F30");
+    } else {
+      setFindBinsButtonColor("#515F30");
+      setAddBinsButtonColor("#8BA352");
     }
-    function changeAddButtonColor() {
-        if(addBinsButtonColor == "#515F30") {
-            setAddBinsButtonColor("#8BA352");
-            setFindBinsButtonColor("#515F30");
-        } else {
-            setAddBinsButtonColor("#515F30");
-            setFindBinsButtonColor("#8BA352");
-        }
+  }
+  function changeAddButtonColor() {
+    if (addBinsButtonColor == "#515F30") {
+      setAddBinsButtonColor("#8BA352");
+      setFindBinsButtonColor("#515F30");
+    } else {
+      setAddBinsButtonColor("#515F30");
+      setFindBinsButtonColor("#8BA352");
     }
-    
-    return (
-        <nav>
-            <img className="logo" src="Logo.png"/>
-            <ul className="nav-links">
-            
-                <Link to='/findbins' > 
-                    <button style={{backgroundColor : findBinsButtonColor}} className="find-bins-button" onClick={changeFindButtonColor}>
-                    Find Bins
-                    </button>
-                </Link>
-                <Link to='/addbins'> 
-                    <button style={{backgroundColor : addBinsButtonColor}} className="add-bins-button" onClick={changeAddButtonColor}>
-                    Add Bins
-                    </button>
-                </Link>
-            </ul>
-        </nav>
-    );
-    
+  }
+
+  return (
+    <nav>
+      <container className="nav-links">
+        <img className="logo" src="Logo.png" />
+
+        <Link to="/findbins">
+          <button
+            style={{ backgroundColor: findBinsButtonColor }}
+            className="find-bins-button btn"
+            onClick={changeFindButtonColor}
+          >
+            Find Bins
+          </button>
+        </Link>
+        <Link to="/addbins">
+          <button
+            style={{ backgroundColor: addBinsButtonColor }}
+            className="add-bins-button btn"
+            onClick={changeAddButtonColor}
+          >
+            Add Bins
+          </button>
+        </Link>
+      </container>
+    </nav>
+  );
 }
-
 
 export default Nav;
